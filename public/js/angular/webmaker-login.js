4102225a45436be00ded5cfa0c472da0ec77e271
Christopher De Cairos <cade@mozillafoundation.org>
2014-08-06 12:30:23 -0400

    yay

--- public/js/angular/webmaker-login.js
+++ public/js/angular/webmaker-login.js
@@ -29,7 +29,7 @@ angular
         apply();
       });
 
-      auth.on('tokenlogin', function(user) {
+      auth.on('tokenlogin', function (user) {
         $rootScope._user = user;
         apply();
       });
@@ -40,62 +40,107 @@ angular
       });
 
       auth.on('error', function (message, xhr) {
-        console.log('error', message, xhr);
+        console.error('error', message, xhr);
       });
 
       return auth;
     }
   ])
-  .controller('tokenLoginController', ['$scope', '$http', '$modal', 'webmakerLoginService',
-    function ($scope, $http, $modal, webmakerLoginService) {
-      $scope.tokenLogin = function() {
+  .controller('tokenLoginController', ['$rootScope', '$scope', '$http', '$modal', 'webmakerLoginService',
+    function ($rootScope, $scope, $http, $modal, webmakerLoginService) {
+      $rootScope.tokenLogin = function (email) {
         $modal.open({
           templateUrl: '/views/token-sign-in.html',
-          controller: tokenLogin
+          controller: tokenLogin,
+          resolve: {
+            email: function() {
+              return email;
+            }
+          }
         });
-      }
+      };
 
-      var tokenLogin = function($scope, $modalInstance, webmakerLoginService) {
+      var tokenLogin = function ($scope, $modalInstance, email) {
         $scope.form = {};
         $scope.user = {};
 
+        if ( email ) {
+          $scope.user.loginEmail = email;
+        }
+
         var emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-        $scope.submitEmail = function() {
+        $scope.checkEmail = function () {
+          if (!$scope.user.loginEmail) {
+            return;
+          }
+
+          var isValid = emailRegex.test($scope.user.loginEmail);
+
+          $scope.form.user.loginEmail.$setValidity('invalid', isValid);
+          $scope.form.user.loginEmail.$setValidity('accountExists', true);
+
+          if (!isValid) {
+            return;
+          }
+
+          $http
+            .get(webmakerLoginService.urls.checkEmail + $scope.user.loginEmail)
+            .success(function (email) {
+              $scope.form.user.loginEmail.$setValidity('accountExists', email.exists);
+            })
+            .error(function (err) {
+              $scope.form.user.loginEmail.$setValidity('accountExists', true);
+            });
+        };
+
+        $scope.submitEmail = function () {
           var isValid = emailRegex.test($scope.user.loginEmail);
           $scope.form.user.loginEmail.$setValidity("invalid", isValid);
-          if ( !isValid ) {
+          if (!isValid) {
             return;
           }
           webmakerLoginService.request($scope.user.loginEmail);
           $scope.enterToken = true;
         };
 
-        $scope.submitToken = function() {
+        $scope.submitToken = function () {
           webmakerLoginService.authenticateToken($scope.user.loginEmail, $scope.user.token);
         };
 
-        webmakerLoginService.on('tokenlogin', function() {
+        $scope.cancel = function () {
+          $modalInstance.dismiss('cancel');
+        };
+
+        webmakerLoginService.on('tokenlogin', function () {
           $modalInstance.dismiss('done');
         });
-
       };
     }
   ])
-  .controller('createUserController', ['$scope', '$http', '$modal', 'webmakerLoginService',
-    function ($scope, $http, $modal, webmakerLoginService) {
+  .controller('createUserController', ['$rootScope', '$scope', '$http', '$modal', 'webmakerLoginService',
+    function ($rootScope, $scope, $http, $modal, webmakerLoginService) {
 
-      $scope.createUserTwo = function () {
+      $rootScope.createUserTwo = function (email) {
         $modal.open({
           templateUrl: '/views/create-user-form-2.html',
-          controller: createUserCtrlTwo
+          controller: createUserCtrlTwo,
+          resolve: {
+            email: function() {
+              return email;
+            }
+          }
         });
       };
 
-      var createUserCtrlTwo = function ($scope, $modalInstance, webmakerLoginService) {
+      var createUserCtrlTwo = function ($scope, $modalInstance, email) {
 
         $scope.form = {};
         $scope.user = {};
+
+        if ( email ) {
+          $scope.user.email = email;
+        }
 
         var usernameRegex = /^[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\-]{1,20}$/;
         var emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
@@ -106,7 +151,7 @@ angular
 
         $scope.submitEmail = function () {
           $scope.submit = true;
-          if ( $scope.form.agree && $scope.user.email ) {
+          if ($scope.form.agree && $scope.user.email) {
             $scope.enterEmail = false;
             $scope.selectUsername = true;
           }
@@ -122,16 +167,16 @@ angular
           $scope.form.user.email.$setValidity('invalid', isValid);
           $scope.form.user.email.$setValidity('accountExists', true);
 
-          if ( !isValid ) {
+          if (!isValid) {
             return;
           }
 
           $http
             .get(webmakerLoginService.urls.checkEmail + $scope.user.email)
-            .success(function(email) {
+            .success(function (email) {
               $scope.form.user.email.$setValidity('accountExists', !email.exists);
             })
-            .error(function(err) {
+            .error(function (err) {
               $scope.form.user.email.$setValidity('accountExists', true);
             });
         };
@@ -140,9 +185,12 @@ angular
           if ($scope.form.user.$valid && $scope.form.agree) {
             webmakerLoginService.createUser2({
               user: $scope.user
-            }, function(err) {
+            }, function (err, user) {
               $scope.selectUsername = false;
               $scope.welcome = true;
+              if (!$rootScope.$$phase) {
+                $rootScope.$apply();
+              }
             });
           }
         };
